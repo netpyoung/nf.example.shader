f965cac95693242722c031651213b6245f8ca0dd
netpyoung <netpyoung@gmail.com>
2023-09-24 01:46:43 +0900

    fix: print_depth example

--- nf.example.shader/Assets/30_SRP/print_depth/Feature_PrintDepthMap.cs
+++ nf.example.shader/Assets/30_SRP/print_depth/Feature_PrintDepthMap.cs
@@ -6,20 +6,40 @@ public class Feature_PrintDepthMap : ScriptableRendererFeature
 {
     class Pass_PrintDepthMap : ScriptableRenderPass
     {
-        const string RENDER_TAG = "PrintDepthMapPass";
+        const string RENDER_TAG = nameof(Pass_PrintDepthMap);
 
-        Volume_PrintDepthMap _depthMap;
-        Material _material;
-        RenderTargetIdentifier _currentTarget;
-        RenderTargetHandle _destination;
+        private readonly Material _material;
+        private Volume_PrintDepthMap _depthMap;
+        private RTHandle _cameraColorTargetHandle;
 
         public Pass_PrintDepthMap(Material material)
         {
             _material = material;
         }
 
+        internal void Setup(RTHandle cameraColorTargetHandle)
+        {
+            _cameraColorTargetHandle = cameraColorTargetHandle;
+        }
+
+        public override void OnCameraSetup(CommandBuffer cmd, ref RenderingData renderingData)
+        {
+            ConfigureTarget(_cameraColorTargetHandle);
+        }
+
         public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
         {
+            CameraData cameraData = renderingData.cameraData;
+            if (cameraData.camera.cameraType != CameraType.Game)
+            {
+                return;
+            }
+
+            if (_material == null)
+            {
+                return;
+            }
+
             if (!renderingData.cameraData.postProcessEnabled)
             {
                 return;
@@ -39,41 +59,35 @@ public class Feature_PrintDepthMap : ScriptableRendererFeature
             }
 
             CommandBuffer cmd = CommandBufferPool.Get(RENDER_TAG);
-            Render(cmd, ref renderingData);
+            Blitter.BlitCameraTexture(cmd, _cameraColorTargetHandle, _cameraColorTargetHandle, _material, 0);
             context.ExecuteCommandBuffer(cmd);
             CommandBufferPool.Release(cmd);
         }
-
-        void Render(CommandBuffer cmd, ref RenderingData renderingData)
-        {
-            if (renderingData.cameraData.isSceneViewCamera)
-            {
-                return;
-            }
-
-            Blit(cmd, ref renderingData, _material);
-        }
-
-        internal void Setup(RenderTargetIdentifier cameraColorTarget, RenderTargetHandle cameraHandle)
-        {
-            this._currentTarget = cameraColorTarget;
-            this._destination = cameraHandle;
-        }
     }
 
-    Pass_PrintDepthMap _pass;
     public Material Material;
+    private Pass_PrintDepthMap _pass;
 
     public override void Create()
     {
         _pass = new Pass_PrintDepthMap(Material);
-        _pass.renderPassEvent = RenderPassEvent.AfterRenderingPostProcessing;
+        _pass.renderPassEvent = RenderPassEvent.BeforeRenderingPostProcessing;
     }
 
     public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
     {
-        RenderTargetHandle cameraHandle = RenderTargetHandle.CameraTarget;
-        _pass.Setup(renderer.cameraColorTarget, cameraHandle);
-        renderer.EnqueuePass(_pass);
+        if (renderingData.cameraData.cameraType == CameraType.Game)
+        {
+            renderer.EnqueuePass(_pass);
+        }
+    }
+
+    public override void SetupRenderPasses(ScriptableRenderer renderer, in RenderingData renderingData)
+    {
+        if (renderingData.cameraData.cameraType == CameraType.Game)
+        {
+            _pass.ConfigureInput(ScriptableRenderPassInput.Color);
+            _pass.Setup(renderer.cameraColorTargetHandle);
+        }
     }
 }
--- nf.example.shader/Assets/30_SRP/print_depth/print_depth.shader
+++ nf.example.shader/Assets/30_SRP/print_depth/print_depth.shader
@@ -2,71 +2,27 @@
 {
     SubShader
     {
-        Tags
+        Pass // 0
         {
-            "RenderPipeline" = "UniversalRenderPipeline"
-            "Queue" = "Geometry"
-            "RenderType" = "Opaque"
-        }
+            NAME "PRINT_DEPTH"
 
-        Pass
-        {
-            Tags
-            {
-                "LightMode" = "UniversalForward"
-            }
-            
-            Cull Off
-            ZTest Always
+            Cull Back
             ZWrite Off
+            ZTest Always
 
             HLSLPROGRAM
             #pragma target 3.5
 
-            #pragma vertex vert
             #pragma fragment frag
 
             #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
             #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/DeclareDepthTexture.hlsl"
-
-            half4 _MainTex_TexelSize;
-
-            struct APPtoVS
+            #include "Packages/com.unity.render-pipelines.core/Runtime/Utilities/Blit.hlsl"
+            #pragma vertex Vert
+            
+            half4 frag(Varyings IN) : SV_Target
             {
-                float4 positionOS   : POSITION;
-                float2 uv           : TEXCOORD0;
-            };
-
-            struct VStoFS
-            {
-                float4 positionCS   : SV_POSITION;
-                float2 uv           : TEXCOORD0;
-            };
-
-            VStoFS vert(APPtoVS IN)
-            {
-                VStoFS OUT;
-                ZERO_INITIALIZE(VStoFS, OUT);
-
-                OUT.positionCS = TransformObjectToHClip(IN.positionOS.xyz);
-                OUT.uv = IN.uv;
-
-                #if UNITY_UV_STARTS_AT_TOP
-                    // _MainTex_TexelSize.xy    : x= 1/°¡·Î, y= 1/¼¼·Î.
-                    // UNITY_UV_STARTS_AT_TOP   : DirectX == 1, OpenGL == 0.
-                    // _MainTex_TexelSize.y < 0 : antialiasing On.
-                    if (_MainTex_TexelSize.y < 0)
-                    {
-                        OUT.uv.y = 1 - OUT.uv.y;
-                    }
-                #endif
-
-                return OUT;
-            }
-
-            half4 frag(VStoFS IN) : SV_Target
-            {
-                half depth = SampleSceneDepth(IN.uv);
+                half depth = SampleSceneDepth(IN.texcoord);
 
                 half linear01Depth = Linear01Depth(depth, _ZBufferParams);
 
