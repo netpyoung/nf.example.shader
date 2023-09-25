2dce962e592fbda26043f3707392a4667be20d11
netpyoung <netpyoung@gmail.com>
2023-09-26 06:40:06 +0900

    fix: BloomDualFilter

--- nf.example.srp/Assets/BloomDualFilter/BloomDualFilter_RenderPassFeature.cs
+++ nf.example.srp/Assets/BloomDualFilter/BloomDualFilter_RenderPassFeature.cs
@@ -1,149 +1,214 @@
+using System;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 
 public class BloomDualFilter_RenderPassFeature : ScriptableRendererFeature
 {
-    class BloomDualFilter_RenderPass : ScriptableRenderPass
+    private Pass_BloomDualFilter _pass;
+    public Material MaterialBloom;
+    public Material MaterialDualFilter;
+    public int DualFilterStep;
+
+    private RTCollection _rtCollection;
+
+    protected override void Dispose(bool disposing)
     {
-        const string RENDER_TAG = nameof(BloomDualFilter_RenderPass);
-        
-        const int PASS_BLOOM_THRESHOLD = 0;
-        const int PASS_BLOOM_COMBINE = 1;
+        _rtCollection.Dispose();
+    }
 
-        const int PASS_DUALFILTER_DOWN = 0;
-        const int PASS_DUALFILTER_UP = 1;
+    public override void Create()
+    {
+        DualFilterStep = Math.Max(DualFilterStep, 0);
 
-        public int downsample = 8;
+        _rtCollection = new RTCollection(DualFilterStep);
+        _pass = new Pass_BloomDualFilter(_rtCollection, MaterialBloom, MaterialDualFilter);
+        _pass.renderPassEvent = RenderPassEvent.BeforeRenderingPostProcessing;
+    }
 
-        Material _materialBloom;
-        Material _materialDualFilter;
-
-        RenderTargetIdentifier _source;
-        RenderTargetIdentifier _destination;
-
-        readonly static int _BloomBrightTex = Shader.PropertyToID("_BloomNonBlurTex");
-        readonly static int _DualFilterDownTex = Shader.PropertyToID("_DualFilterDownTex");
-        readonly static int _DualFilterDownTex1 = Shader.PropertyToID("_DualFilterDownTex1");
-        readonly static int _DualFilterDownTex2 = Shader.PropertyToID("_DualFilterDownTex2");
-        readonly static int _DualFilterDownTex3 = Shader.PropertyToID("_DualFilterDownTex3");
-        readonly static int _DualFilterUpTex3 = Shader.PropertyToID("_BloomBlurTex3");
-        readonly static int _DualFilterUpTex2 = Shader.PropertyToID("_BloomBlurTex2");
-        readonly static int _DualFilterUpTex1 = Shader.PropertyToID("_BloomBlurTex1");
-        readonly static int _DualFilterUpTex = Shader.PropertyToID("_BloomBlurTex");
-
-
-        RenderTargetIdentifier _bloomBrightRT;
-        RenderTargetIdentifier _dualFilterDownRT;
-        RenderTargetIdentifier _dualFilterUpRT;
-        RenderTargetIdentifier _dualFilterDownRT1;
-        RenderTargetIdentifier _dualFilterUpRT1;
-        RenderTargetIdentifier _dualFilterDownRT2;
-        RenderTargetIdentifier _dualFilterUpRT2;
-        RenderTargetIdentifier _dualFilterDownRT3;
-        RenderTargetIdentifier _dualFilterUpRT3;
-
-        public BloomDualFilter_RenderPass(Material materialBloom, Material materialDualFilter)
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+    {
+        if (renderingData.cameraData.cameraType == CameraType.Game)
         {
+            _rtCollection.Setup(renderingData.cameraData.cameraTargetDescriptor);
+            renderer.EnqueuePass(_pass);
+        }
+    }
+
+    public override void SetupRenderPasses(ScriptableRenderer renderer, in RenderingData renderingData)
+    {
+        if (renderingData.cameraData.cameraType == CameraType.Game)
+        {
+            _pass.ConfigureInput(ScriptableRenderPassInput.Color);
+            _pass.Setup(renderer.cameraColorTargetHandle);
+        }
+    }
+
+    class RTCollection : IDisposable
+    {
+        public int _BloomNonBlurTex_Id { get; private set; }
+        public int _BloomBlurTex_Id { get; private set; }
+
+        public RTHandle BloomBrightRT => _bloomBrightRT;
+        public RTHandle[] DualFilterRTs => _dualFilterRTs;
+        public int DualFilterStep { get; private set; }
+
+        private RTHandle _bloomBrightRT;
+        private RTHandle[] _dualFilterRTs;
+        private bool _isInitialized;
+
+        public RTCollection(int desireDualFilterStep)
+        {
+            _BloomNonBlurTex_Id = Shader.PropertyToID("_BloomNonBlurTex");
+            _BloomBlurTex_Id = Shader.PropertyToID("_BloomBlurTex");
+            DualFilterStep = desireDualFilterStep;
+        }
+
+        internal void Setup(RenderTextureDescriptor mainDesc)
+        {
+            if (_isInitialized)
+            {
+                return;
+            }
+            _isInitialized = true;
+
+            RenderTextureFormat tf = RenderTextureFormat.ARGB32;
+            RenderTextureDescriptor rtdesc = new RenderTextureDescriptor(mainDesc.width, mainDesc.height, tf, 0);
+
+            RenderingUtils.ReAllocateIfNeeded(ref _bloomBrightRT, rtdesc, FilterMode.Bilinear, TextureWrapMode.Clamp,
+                name: "_MyColorTexture");
+
+            int fromDiv = 2;
+            int measureStep = _MeasureStep(mainDesc.width / fromDiv, mainDesc.height / fromDiv);
+            DualFilterStep = Math.Min(measureStep, DualFilterStep);
+            _dualFilterRTs = new RTHandle[DualFilterStep];
+
+            for (int i = 0; i < DualFilterStep; ++i)
+            {
+                int div = 1 << i; // 1, 2, 4, 8 ...
+                div = div * fromDiv; // 8, 16, 32, 64 ...
+                int w = mainDesc.width / div;
+                int h = mainDesc.height / div;
+                RenderTextureDescriptor desc = new RenderTextureDescriptor(w, h, tf, 0);
+                RenderingUtils.ReAllocateIfNeeded(ref _dualFilterRTs[i], desc, FilterMode.Bilinear, TextureWrapMode.Clamp,
+                                name: $"_dualFilterRTs_{i}");
+            }
+        }
+
+        int _MeasureStep(int width, int height)
+        {
+            int min = Math.Min(width, height);
+            int step = 0;
+            while (min > 1)
+            {
+                min >>= 1;
+                step++;
+            }
+            step++;
+            return step;
+        }
+
+        public void Dispose()
+        {
+            if (!_isInitialized)
+            {
+                return;
+            }
+
+            RTHandles.Release(_bloomBrightRT);
+            for (int i = 0; i < DualFilterStep; ++i)
+            {
+                RTHandles.Release(_dualFilterRTs[i]);
+            }
+        }
+    }
+
+    class Pass_BloomDualFilter : ScriptableRenderPass
+    {
+        const string RENDER_TAG = nameof(Pass_BloomDualFilter);
+        const int BLOOM_THRESHOLD_PASS = 0;
+        const int BLOOM_COMBINE_PASS = 1;
+        const int DUALFILTER_DOWN_PASS = 0;
+        const int DUALFILTER_UP_PASS = 1;
+
+        private Material _materialBloom;
+        private Material _materialDualFilter;
+
+        private RTHandle _cameraColorTargetHandle;
+        private RTCollection _rtc;
+
+        public Pass_BloomDualFilter(RTCollection rtCollection, Material materialBloom, Material materialDualFilter)
+        {
+            _rtc = rtCollection;
             _materialBloom = materialBloom;
             _materialDualFilter = materialDualFilter;
         }
 
+        internal void Setup(RTHandle cameraColorTargetHandle)
+        {
+            _cameraColorTargetHandle = cameraColorTargetHandle;
+        }
+
         public override void OnCameraSetup(CommandBuffer cmd, ref RenderingData renderingData)
         {
-            this._source = renderingData.cameraData.renderer.cameraColorTarget;
-            this._destination = RenderTargetHandle.CameraTarget.Identifier();
+            ConfigureTarget(_cameraColorTargetHandle);
         }
 
         public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
         {
-            if (renderingData.cameraData.isSceneViewCamera)
+            CameraData cameraData = renderingData.cameraData;
+            if (cameraData.camera.cameraType != CameraType.Game)
+            {
+                return;
+            }
+
+            if (_materialBloom == null)
+            {
+                return;
+            }
+
+            if (_materialDualFilter == null)
+            {
+                return;
+            }
+
+            if (!renderingData.cameraData.postProcessEnabled)
             {
                 return;
             }
 
             CommandBuffer cmd = CommandBufferPool.Get(RENDER_TAG);
-
-
-            cmd.Blit(_source, _bloomBrightRT, _materialBloom, PASS_BLOOM_THRESHOLD);
-
-            cmd.Blit(_bloomBrightRT, _dualFilterDownRT, _materialDualFilter);
-            cmd.Blit(_dualFilterDownRT, _dualFilterDownRT1, _materialDualFilter, PASS_DUALFILTER_DOWN);
-            cmd.Blit(_dualFilterDownRT1, _dualFilterDownRT2, _materialDualFilter, PASS_DUALFILTER_DOWN);
-            cmd.Blit(_dualFilterDownRT2, _dualFilterDownRT3, _materialDualFilter, PASS_DUALFILTER_DOWN);
-
-            cmd.Blit(_dualFilterDownRT3, _dualFilterUpRT3, _materialDualFilter, PASS_DUALFILTER_UP);
-            cmd.Blit(_dualFilterUpRT3, _dualFilterUpRT2, _materialDualFilter, PASS_DUALFILTER_UP);
-            cmd.Blit(_dualFilterUpRT2, _dualFilterUpRT1, _materialDualFilter, PASS_DUALFILTER_UP);
-            cmd.Blit(_dualFilterUpRT1, _dualFilterUpRT, _materialDualFilter);
-
-            cmd.SetGlobalTexture(_BloomBrightTex, _bloomBrightRT);
-            cmd.SetGlobalTexture(_DualFilterUpTex, _dualFilterUpRT);
-
-            cmd.Blit(_source, _destination, _materialBloom, PASS_BLOOM_COMBINE);
-
+            Render(cmd, ref renderingData);
             context.ExecuteCommandBuffer(cmd);
-            cmd.Clear();
             CommandBufferPool.Release(cmd);
         }
 
-        public override void Configure(CommandBuffer cmd, RenderTextureDescriptor cameraTextureDescriptor)
+        void Render(CommandBuffer cmd, ref RenderingData renderingData)
         {
-            var width = cameraTextureDescriptor.width;
-            var height = cameraTextureDescriptor.height;
+            if (_rtc.DualFilterStep < 1)
+            {
+                return;
+            }
 
+            Blitter.BlitCameraTexture(cmd, _cameraColorTargetHandle, _rtc.BloomBrightRT, _materialBloom, BLOOM_THRESHOLD_PASS);
+            Blitter.BlitCameraTexture(cmd, _rtc.BloomBrightRT, _rtc.DualFilterRTs[0], _materialDualFilter, DUALFILTER_DOWN_PASS);
 
-            RenderTextureFormat tf = RenderTextureFormat.ARGB32;
+            for (int i = 0; i < _rtc.DualFilterStep / 2 - 1; ++i)
+            {
+                Blitter.BlitCameraTexture(cmd, _rtc.DualFilterRTs[i], _rtc.DualFilterRTs[i + 1], _materialDualFilter, DUALFILTER_DOWN_PASS);
+            }
+            for (int i = _rtc.DualFilterStep / 2 - 1; i > 0; --i)
+            {
+                Blitter.BlitCameraTexture(cmd, _rtc.DualFilterRTs[i], _rtc.DualFilterRTs[i - 1], _materialDualFilter, DUALFILTER_UP_PASS);
+            }
 
-            cmd.GetTemporaryRT(_BloomBrightTex, width / 4, height / 4, 0, FilterMode.Bilinear, tf);
+            //Blitter.BlitCameraTexture(cmd, _bloomBrightRT, _dualFilterDownRT, _materialDualFilter, DUALFILTER_DOWN_PASS);
+            //Blitter.BlitCameraTexture(cmd, _dualFilterDownRT, _dualFilterUpRT, _materialDualFilter, DUALFILTER_UP_PASS);
 
-            cmd.GetTemporaryRT(_DualFilterDownTex, width / 8, height / 8, 0, FilterMode.Bilinear, tf);
-            cmd.GetTemporaryRT(_DualFilterDownTex1, width / 16, height / 16, 0, FilterMode.Bilinear, tf);
-            cmd.GetTemporaryRT(_DualFilterDownTex2, width / 32, height / 32, 0, FilterMode.Bilinear, tf);
-            cmd.GetTemporaryRT(_DualFilterDownTex3, width / 64, height / 64, 0, FilterMode.Bilinear, tf);
+            cmd.SetGlobalTexture(_rtc._BloomNonBlurTex_Id, _rtc.BloomBrightRT);
+            cmd.SetGlobalTexture(_rtc._BloomBlurTex_Id, _rtc.DualFilterRTs[0]);
 
-            cmd.GetTemporaryRT(_DualFilterUpTex3, width / 32, height / 32, 0, FilterMode.Bilinear, tf);
-            cmd.GetTemporaryRT(_DualFilterUpTex2, width / 16, height / 16, 0, FilterMode.Bilinear, tf);
-            cmd.GetTemporaryRT(_DualFilterUpTex1, width / 8, height / 8, 0, FilterMode.Bilinear, tf);
-            cmd.GetTemporaryRT(_DualFilterUpTex, width / 4, height / 4, 0, FilterMode.Bilinear, tf);
-
-            _bloomBrightRT = new RenderTargetIdentifier(_BloomBrightTex);
-            _dualFilterDownRT = new RenderTargetIdentifier(_DualFilterDownTex);
-            _dualFilterDownRT1 = new RenderTargetIdentifier(_DualFilterDownTex1);
-            _dualFilterDownRT2 = new RenderTargetIdentifier(_DualFilterDownTex2);
-            _dualFilterDownRT3 = new RenderTargetIdentifier(_DualFilterDownTex3);
-            _dualFilterUpRT3 = new RenderTargetIdentifier(_DualFilterUpTex3);
-            _dualFilterUpRT2 = new RenderTargetIdentifier(_DualFilterUpTex2);
-            _dualFilterUpRT1 = new RenderTargetIdentifier(_DualFilterUpTex1);
-            _dualFilterUpRT = new RenderTargetIdentifier(_DualFilterUpTex);
+            Blitter.BlitCameraTexture(cmd, _cameraColorTargetHandle, _cameraColorTargetHandle, _materialBloom, BLOOM_COMBINE_PASS);
         }
-
-        public override void OnCameraCleanup(CommandBuffer cmd)
-        {
-            cmd.ReleaseTemporaryRT(_BloomBrightTex);
-            cmd.ReleaseTemporaryRT(_DualFilterDownTex);
-            cmd.ReleaseTemporaryRT(_DualFilterDownTex1);
-            cmd.ReleaseTemporaryRT(_DualFilterDownTex2);
-            cmd.ReleaseTemporaryRT(_DualFilterDownTex3);
-            cmd.ReleaseTemporaryRT(_DualFilterUpTex3);
-            cmd.ReleaseTemporaryRT(_DualFilterUpTex2);
-            cmd.ReleaseTemporaryRT(_DualFilterUpTex1);
-            cmd.ReleaseTemporaryRT(_DualFilterUpTex);
-        }
-    }
-
-    BloomDualFilter_RenderPass _pass;
-    public Material MaterialBloom;
-    public Material MaterialDualFilter;
-
-    public override void Create()
-    {
-        _pass = new BloomDualFilter_RenderPass(MaterialBloom, MaterialDualFilter);
-        _pass.renderPassEvent = RenderPassEvent.AfterRenderingPostProcessing;
-    }
-
-    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
-    {
-        renderer.EnqueuePass(_pass);
     }
 }
--- nf.example.srp/Assets/BloomDualFilter/DualFilter.shader
+++ nf.example.srp/Assets/BloomDualFilter/DualFilter.shader
@@ -2,68 +2,38 @@
 {
     // ref: [SIGGRAPH2015 - Bandwidth-efficient Graphics](https://community.arm.com/cfs-file/__key/communityserver-blogs-components-weblogfiles/00-00-00-20-66/siggraph2015_2D00_mmg_2D00_marius_2D00_notes.pdf)
 
-    Properties
-    {
-        [HideInInspector] _MainTex("UI Texture", 2D) = "white" {}
-    }
-
     SubShader
     {
+        Cull Back
+        ZWrite Off
+        ZTest Off
+
+        HLSLINCLUDE
+        #pragma target 3.5
+        #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
+        #include "Packages/com.unity.render-pipelines.core/Runtime/Utilities/Blit.hlsl"
+        #pragma vertex Vert
+        #pragma fragment frag
+        float4 _BlitTexture_TexelSize;
+
+        ENDHLSL
+
         Pass // 0
         {
             NAME "DUALFILTER_DOWN"
 
-            Cull Back
-            ZWrite Off
-            ZTest Off
-
             HLSLPROGRAM
-            #pragma target 3.5
-
-            #pragma vertex vert
-            #pragma fragment frag
-
-            #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
-
-            TEXTURE2D(_MainTex);    SAMPLER(sampler_MainTex);
-
-            CBUFFER_START(UnityPerMaterial)
-            float4 _MainTex_ST;
-            float4 _MainTex_TexelSize;
-            CBUFFER_END
-
-            struct APPtoVS
+            half4 frag(Varyings IN) : SV_Target
             {
-                float4 positionOS   : POSITION;
-                float2 uv           : TEXCOORD0;
-            };
-
-            struct VStoFS
-            {
-                float4 positionCS   : SV_POSITION;
-                float2 uv           : TEXCOORD0;
-            };
-
-            VStoFS vert(APPtoVS IN)
-            {
-                VStoFS OUT;
-                ZERO_INITIALIZE(VStoFS, OUT);
-                OUT.positionCS = TransformObjectToHClip(IN.positionOS.xyz);
-                OUT.uv = IN.uv;
-                return OUT;
-            }
-
-            half4 frag(VStoFS IN) : SV_Target
-            {
-                float2 res = _MainTex_TexelSize.xy;
-                float i = 0.5;
+                float2 res = _BlitTexture_TexelSize.xy;
+                float i = 0.5; // scatter
 
                 half3 color;
-                color = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, IN.uv).rgb * 4.0;
-                color += SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, IN.uv + float2(i, i) * res).rgb;
-                color += SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, IN.uv + float2(i, -i) * res).rgb;
-                color += SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, IN.uv + float2(-i, i) * res).rgb;
-                color += SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, IN.uv + float2(-i, -i) * res).rgb;
+                color = SAMPLE_TEXTURE2D(_BlitTexture, sampler_PointClamp, IN.texcoord).rgb * 4.0;
+                color += SAMPLE_TEXTURE2D(_BlitTexture, sampler_PointClamp, IN.texcoord + float2(i, i) * res).rgb;
+                color += SAMPLE_TEXTURE2D(_BlitTexture, sampler_PointClamp, IN.texcoord + float2(i, -i) * res).rgb;
+                color += SAMPLE_TEXTURE2D(_BlitTexture, sampler_PointClamp, IN.texcoord + float2(-i, i) * res).rgb;
+                color += SAMPLE_TEXTURE2D(_BlitTexture, sampler_PointClamp, IN.texcoord + float2(-i, -i) * res).rgb;
                 color *= 0.125; // color /= 8.0;
 
                 return half4(color, 1);
@@ -75,60 +45,21 @@
         {
             NAME "DUALFILTER_UP"
 
-            Cull Back
-            ZWrite Off
-            ZTest Off
-
             HLSLPROGRAM
-            #pragma target 3.5
-
-            #pragma vertex vert
-            #pragma fragment frag
-
-            #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
-
-            TEXTURE2D(_MainTex);    SAMPLER(sampler_MainTex);
-
-            CBUFFER_START(UnityPerMaterial)
-            float4 _MainTex_ST;
-            float4 _MainTex_TexelSize;
-            CBUFFER_END
-
-            struct APPtoVS
+            half4 frag(Varyings IN) : SV_Target
             {
-                float4 positionOS   : POSITION;
-                float2 uv           : TEXCOORD0;
-            };
-
-            struct VStoFS
-            {
-                float4 positionCS   : SV_POSITION;
-                float2 uv           : TEXCOORD0;
-            };
-
-            VStoFS vert(APPtoVS IN)
-            {
-                VStoFS OUT;
-                ZERO_INITIALIZE(VStoFS, OUT);
-                OUT.positionCS = TransformObjectToHClip(IN.positionOS.xyz);
-                OUT.uv = IN.uv;
-                return OUT;
-            }
-
-            half4 frag(VStoFS IN) : SV_Target
-            {
-                float2 res = _MainTex_TexelSize.xy;
+                float2 res = _BlitTexture_TexelSize.xy;
                 float i = 0.5;
-
-                half3 color = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, IN.uv + float2(i * 2, 0) * res).rgb;
-                color += SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, IN.uv + float2(-i * 2, 0) * res).rgb;
-                color += SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, IN.uv + float2(0, i * 2) * res).rgb;
-                color += SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, IN.uv + float2(0, -i * 2) * res).rgb;
                 
-                color += SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, IN.uv + float2(i, i) * res).rgb * 2.0;
-                color += SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, IN.uv + float2(-i, i) * res).rgb * 2.0;
-                color += SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, IN.uv + float2(i, -i) * res).rgb * 2.0;
-                color += SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, IN.uv + float2(-i, -i) * res).rgb * 2.0;
+                half3 color = SAMPLE_TEXTURE2D(_BlitTexture, sampler_PointClamp, IN.texcoord + float2(i * 2, 0) * res).rgb;
+                color += SAMPLE_TEXTURE2D(_BlitTexture, sampler_PointClamp, IN.texcoord + float2(-i * 2, 0) * res).rgb;
+                color += SAMPLE_TEXTURE2D(_BlitTexture, sampler_PointClamp, IN.texcoord + float2(0, i * 2) * res).rgb;
+                color += SAMPLE_TEXTURE2D(_BlitTexture, sampler_PointClamp, IN.texcoord + float2(0, -i * 2) * res).rgb;
+                
+                color += SAMPLE_TEXTURE2D(_BlitTexture, sampler_PointClamp, IN.texcoord + float2(i, i) * res).rgb * 2.0;
+                color += SAMPLE_TEXTURE2D(_BlitTexture, sampler_PointClamp, IN.texcoord + float2(-i, i) * res).rgb * 2.0;
+                color += SAMPLE_TEXTURE2D(_BlitTexture, sampler_PointClamp, IN.texcoord + float2(i, -i) * res).rgb * 2.0;
+                color += SAMPLE_TEXTURE2D(_BlitTexture, sampler_PointClamp, IN.texcoord + float2(-i, -i) * res).rgb * 2.0;
 
                 color *= 0.08334; // color /= 12.0;
 
